de346cf30f4183631d6605ce156e18ee783896b2
bangnguyen794 <bangnguyen794@gmail.com>
2023-02-26 18:23:37 +0700

    #7 ..ToInt, isUndefined ..vv

--- src/units/cus.validator.js
+++ src/units/cus.validator.js
@@ -1,7 +1,28 @@
 var that = module.exports = {
 
         is_phone: (phone)=>{
-            console.log(phone.lenght==10);
-            return (phone.lenght==10);
+            let phone = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+            return phone.test(string_value);
+        },
+        isNullOrUndefinedOrEmpty:(val)=>{
+            return (val===null || val===undefined || val==='');
+        },
+        isNullOrUndefined:(val)=>{
+            return (val===null || val===undefined);
+        },
+        isNull:(val)=>{
+            return (val===null);
+        },
+        isUndefined:(val)=>{
+            return (val===undefined);
+        },
+        ToInt:(val)=>{
+            if(val===null || val===undefined || val==='')
+            return 0;
+            else {
+                const result = parseInt(value, 10);
+                return isNaN(result)?0:result;
+            } 
         }
+        
 }      
